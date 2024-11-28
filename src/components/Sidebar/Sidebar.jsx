9a799a1a0d5efde74288b234153c16052858c637
runinbk <runinbk@gmail.com>
2024-11-28 00:25:46 -0400

    correccion 1 - estilos

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./Sidebar.css";
+import './Sidebar.css';
 
 const Sidebar = ({ isOpen }) => {
   const location = useLocation();
